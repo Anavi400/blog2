e415679be016f1189c40a2e539a020276313fa2b
unknown <ivanafariash@gmail.com>
2024-10-12 12:06:50 -0400

    ajuste

--- registro/views.py
+++ registro/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 
 # Create your views here.
 

460b5439c318c4534da8f602c49a58c9f6b7acee
unknown <ivanafariash@gmail.com>
2024-10-27 13:44:09 -0400

    seccion de comentarios

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -11,3 +12,21 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     text = models.TextField()
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#     def __str__(self) -> str:
+#         return self.text
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author} - {self.content[:20]}'
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,10 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404, redirect
+from .models import Post, Comment
+from django.views.generic.edit import FormMixin
+from .forms import CommentForm
 # Create your views here.
 
 
@@ -38,3 +42,35 @@ class PostUpdateView(UpdateView):
 
 class FirstPageView(TemplateView):
     template_name = "registro/firstpage.html"
+
+
+
+class AddCommentView(FormMixin, DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    form_class = CommentForm
+
+    def get_success_url(self):
+        # Esto redirige de nuevo a la misma página de detalles del post
+        return redirect('post_detail', pk=self.object.pk).url
+
+    def get_context_data(self, **kwargs):
+        # Asegúrate de añadir el formulario al contexto
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.get_form()
+        context['comments'] = self.object.comments.all()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()  # Obtener el objeto Post
+        form = self.get_form()
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = self.object
+            comment.author = request.user
+            comment.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.form_invalid(form)  # Manejo en caso de que el formulario no sea válido
